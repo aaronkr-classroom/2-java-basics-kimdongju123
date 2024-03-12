4446b805d30eee044dee6fd5d18643da392bd016
kimdongju123 <08555@naver.com>
2024-03-12 16:24:39 +0900

    Level 2 final

---
+++ src/example02.java
@@ -0,0 +1,13 @@
+public class example02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+final double PI = 3.1415927;
+
+System.out.println("I'm hungry. I'want pizza.");
+System.out.println("12inch pizza is:");
+System.out.println("Cir:"+PI*12);
+System.out.println("Area: " + PI *6*6);
+
+	}
+
+}
---
+++ src/example03.java
@@ -0,0 +1,36 @@
+public class example03 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte b = 6;
+short s = 2;
+int i = 12;
+long l = 123456789012345L;
+
+char c = 'Y';
+char c2 =88;
+String str = "Do you like pizza?";
+
+float f = 12.34564789f;
+double d = 1234567890.123456789; //
+
+boolean bool = true; //
+//출력하기
+System.out.println(str);
+System.out.println(bool);
+System.out.println(" "+ c);
+System.out.println( ); // 다음 줄로 가기
+System.out.println("\tByte:"+b);
+System.out.println("|Numbers:");
+System.out.println("\short:"+s);
+System.out.println("|int:"+i);
+System.out.println("|Long:"+l);
+System.out.println("|Float:"+f);
+System.out.println("|Double:"+d);
+System.out.println("\nChar2:+ c2");
+System.out.println("\nChar3:+ c3");
+
+
+
+	}
+
+}
